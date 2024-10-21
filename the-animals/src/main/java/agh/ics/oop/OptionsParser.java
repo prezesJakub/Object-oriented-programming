7f1517f12173e21d98ce367130288bca6cfdb756
Jakub Zając <jakubtomaszkrzysztof@gmail.com>
2024-10-21 16:33:53 +0200

    Changed switch in OptionParser

--- the-animals/src/main/java/agh/ics/oop/OptionsParser.java
+++ the-animals/src/main/java/agh/ics/oop/OptionsParser.java
@@ -8,11 +8,15 @@ public class OptionsParser {
     public static MoveDirection[] parse(String[] args) {
         List<MoveDirection> directionList = new ArrayList<>();
         for(int i=0; i<args.length; i++) {
-            switch(args[i]) {
-                case "f" -> directionList.add(MoveDirection.FORWARD);
-                case "b" -> directionList.add(MoveDirection.BACKWARD);
-                case "r" -> directionList.add(MoveDirection.RIGHT);
-                case "l" -> directionList.add(MoveDirection.LEFT);
+            MoveDirection direction = switch(args[i]) {
+                case "f" -> MoveDirection.FORWARD;
+                case "b" -> MoveDirection.BACKWARD;
+                case "r" -> MoveDirection.RIGHT;
+                case "l" -> MoveDirection.LEFT;
+                default -> null;
+            };
+            if(direction != null) {
+                directionList.add(direction);
             }
         }
         return directionList.toArray(new MoveDirection[0]);
